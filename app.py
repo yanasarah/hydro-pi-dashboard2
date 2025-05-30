538082a70cb55b51a211114eecb654737e844cf5
yanasarah <s211021185@studentmail.unimap.edu.my>
2025-05-30 16:03:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="wide")
+
 # ----- CONFIGURE PAGE -----
 st.set_page_config(page_title="Hydro-Pi Dashboard", layout="wide")
 
