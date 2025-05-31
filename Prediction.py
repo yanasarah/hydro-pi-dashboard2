6a0f45686a4362af96583c39049ab4d8d0a6b486
yanasarah <s211021185@studentmail.unimap.edu.my>
2025-05-31 15:49:51 +0800

    Create Prediction.py

---
+++ Prediction.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Prediction", layout="wide")
+st.title("🔮 Plant Growth Prediction")
+
+st.markdown("""
+Use this page to view predicted plant growth trends (Coming soon).
+You can use machine learning here if you want.
+""")
