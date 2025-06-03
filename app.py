9588944f288334f5155e210d6e45c5930b4acb63
yanasarah <s211021185@studentmail.unimap.edu.my>
2025-06-03 23:01:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from streamlit_option_menu import option_menu
+import pandas as pd
+import numpy as np
 
 
 # Set Streamlit page config
